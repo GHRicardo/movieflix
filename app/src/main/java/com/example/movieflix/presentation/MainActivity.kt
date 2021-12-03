4f895d72c5306f96a8d7f4f71fc31de71aa35ceb
ricardovillafuerte <ricardo.vdx@gmail.com>
2021-12-03 16:37:02 -0500

    Se quita el toolbar de MainActivity

--- app/src/main/java/com/example/movieflix/presentation/MainActivity.kt
+++ app/src/main/java/com/example/movieflix/presentation/MainActivity.kt
@@ -2,10 +2,6 @@ package com.example.movieflix.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
-import com.example.movieflix.R
 import com.example.movieflix.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,12 +12,5 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        setSupportActionBar(binding.toolbar)
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
